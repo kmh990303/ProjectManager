d48ee67e88be0f8864b98c0fca5c7c54c31197ea
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-06-24 22:27:31 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+import ProjectsSidebar from "./components/ProjectsSidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
+import NewProject from "./components/NewProject";
+
+function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    })
+  }
+
+  function handleAddProject(ProjectData) {
+    setProjectsState(prevState => {
+      const newProject = {
+        ...ProjectData,
+        id: Math.random()
+      };
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject}/>
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+      {content}
+    </main>
+  );
+}
+
+export default App;
